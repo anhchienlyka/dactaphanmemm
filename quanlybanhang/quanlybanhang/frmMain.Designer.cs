c77d2d323a54d8e7dc4c88f7f28cb25ce2b75076
PCC <PCC@DESKTOP-KH6IVBO>
2021-12-07 07:19:22 +0700

    yu

---
+++ quanlybanhang/quanlybanhang/frmMain.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace quanlybanhang
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.mnuChatLieu = new DevExpress.XtraEditors.SimpleButton();
+            this.mnuNhanVien = new DevExpress.XtraEditors.SimpleButton();
+            this.mnuKhachHang = new DevExpress.XtraEditors.SimpleButton();
+            this.mnuHangHoa = new DevExpress.XtraEditors.SimpleButton();
+            this.mnuHoaDonBan = new DevExpress.XtraEditors.SimpleButton();
+            this.mnuTimKiem = new DevExpress.XtraEditors.SimpleButton();
+            this.mnuBCHangTon = new DevExpress.XtraEditors.SimpleButton();
+            this.mnuBCDoanhThu = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.SuspendLayout();
+            // 
+            // mnuChatLieu
+            // 
+            this.mnuChatLieu.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mnuChatLieu.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.mnuChatLieu.Appearance.Options.UseFont = true;
+            this.mnuChatLieu.Appearance.Options.UseForeColor = true;
+            this.mnuChatLieu.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.mnuChatLieu.Location = new System.Drawing.Point(12, 100);
+            this.mnuChatLieu.Name = "mnuChatLieu";
+            this.mnuChatLieu.Size = new System.Drawing.Size(189, 105);
+            this.mnuChatLieu.TabIndex = 0;
+            this.mnuChatLieu.Text = "Chất Liệu";
+            // 
+            // mnuNhanVien
+            // 
+            this.mnuNhanVien.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mnuNhanVien.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.mnuNhanVien.Appearance.Options.UseFont = true;
+            this.mnuNhanVien.Appearance.Options.UseForeColor = true;
+            this.mnuNhanVien.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.mnuNhanVien.Location = new System.Drawing.Point(258, 100);
+            this.mnuNhanVien.Name = "mnuNhanVien";
+            this.mnuNhanVien.Size = new System.Drawing.Size(189, 105);
+            this.mnuNhanVien.TabIndex = 1;
+            this.mnuNhanVien.Text = "Nhân Viên";
+            // 
+            // mnuKhachHang
+            // 
+            this.mnuKhachHang.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mnuKhachHang.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.mnuKhachHang.Appearance.Options.UseFont = true;
+            this.mnuKhachHang.Appearance.Options.UseForeColor = true;
+            this.mnuKhachHang.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
+            this.mnuKhachHang.Location = new System.Drawing.Point(498, 100);
+            this.mnuKhachHang.Name = "mnuKhachHang";
+            this.mnuKhachHang.Size = new System.Drawing.Size(189, 105);
+            this.mnuKhachHang.TabIndex = 2;
+            this.mnuKhachHang.Text = "Khách hàng";
+            // 
+            // mnuHangHoa
+            // 
+            this.mnuHangHoa.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mnuHangHoa.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.mnuHangHoa.Appearance.Options.UseFont = true;
+            this.mnuHangHoa.Appearance.Options.UseForeColor = true;
+            this.mnuHangHoa.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton4.ImageOptions.Image")));
+            this.mnuHangHoa.Location = new System.Drawing.Point(752, 100);
+            this.mnuHangHoa.Name = "mnuHangHoa";
+            this.mnuHangHoa.Size = new System.Drawing.Size(189, 105);
+            this.mnuHangHoa.TabIndex = 3;
+            this.mnuHangHoa.Text = "Hàng hóa";
+            // 
+            // mnuHoaDonBan
+            // 
+            this.mnuHoaDonBan.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mnuHoaDonBan.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.mnuHoaDonBan.Appearance.Options.UseFont = true;
+            this.mnuHoaDonBan.Appearance.Options.UseForeColor = true;
+            this.mnuHoaDonBan.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton5.ImageOptions.Image")));
+            this.mnuHoaDonBan.Location = new System.Drawing.Point(12, 276);
+            this.mnuHoaDonBan.Name = "mnuHoaDonBan";
+            this.mnuHoaDonBan.Size = new System.Drawing.Size(189, 105);
+            this.mnuHoaDonBan.TabIndex = 4;
+            this.mnuHoaDonBan.Text = "Hóa Đơn Bán";
+            // 
+            // mnuTimKiem
+            // 
+            this.mnuTimKiem.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mnuTimKiem.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.mnuTimKiem.Appearance.Options.UseFont = true;
+            this.mnuTimKiem.Appearance.Options.UseForeColor = true;
+            this.mnuTimKiem.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton6.ImageOptions.Image")));
+            this.mnuTimKiem.Location = new System.Drawing.Point(258, 276);
+            this.mnuTimKiem.Name = "mnuTimKiem";
+            this.mnuTimKiem.Size = new System.Drawing.Size(189, 105);
+            this.mnuTimKiem.TabIndex = 5;
+            this.mnuTimKiem.Text = "Tìm kiếm";
+            // 
+            // mnuBCHangTon
+            // 
+            this.mnuBCHangTon.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mnuBCHangTon.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.mnuBCHangTon.Appearance.Options.UseFont = true;
+            this.mnuBCHangTon.Appearance.Options.UseForeColor = true;
+            this.mnuBCHangTon.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton7.ImageOptions.Image")));
+            this.mnuBCHangTon.Location = new System.Drawing.Point(487, 276);
+            this.mnuBCHangTon.Name = "mnuBCHangTon";
+            this.mnuBCHangTon.Size = new System.Drawing.Size(189, 105);
+            this.mnuBCHangTon.TabIndex = 6;
+            this.mnuBCHangTon.Text = "Báo cáo HH";
+            // 
+            // mnuBCDoanhThu
+            // 
+            this.mnuBCDoanhThu.Appearance.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mnuBCDoanhThu.Appearance.ForeColor = System.Drawing.Color.Blue;
+            this.mnuBCDoanhThu.Appearance.Options.UseFont = true;
+            this.mnuBCDoanhThu.Appearance.Options.UseForeColor = true;
+            this.mnuBCDoanhThu.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton8.ImageOptions.Image")));
+            this.mnuBCDoanhThu.Location = new System.Drawing.Point(752, 276);
+            this.mnuBCDoanhThu.Name = "mnuBCDoanhThu";
+            this.mnuBCDoanhThu.Size = new System.Drawing.Size(189, 105);
+            this.mnuBCDoanhThu.TabIndex = 7;
+            this.mnuBCDoanhThu.Text = "Báo cáo DT";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelControl1.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Appearance.Options.UseForeColor = true;
+            this.labelControl1.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.labelControl1.Location = new System.Drawing.Point(282, 41);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(437, 26);
+            this.labelControl1.TabIndex = 8;
+            this.labelControl1.Text = "CHƯƠNG TRÌNH QUẢN LÝ BÁN HÀNG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 508);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.mnuBCDoanhThu);
+            this.Controls.Add(this.mnuBCHangTon);
+            this.Controls.Add(this.mnuTimKiem);
+            this.Controls.Add(this.mnuHoaDonBan);
+            this.Controls.Add(this.mnuHangHoa);
+            this.Controls.Add(this.mnuKhachHang);
+            this.Controls.Add(this.mnuNhanVien);
+            this.Controls.Add(this.mnuChatLieu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton mnuChatLieu;
+        private DevExpress.XtraEditors.SimpleButton mnuNhanVien;
+        private DevExpress.XtraEditors.SimpleButton mnuKhachHang;
+        private DevExpress.XtraEditors.SimpleButton mnuHangHoa;
+        private DevExpress.XtraEditors.SimpleButton mnuHoaDonBan;
+        private DevExpress.XtraEditors.SimpleButton mnuTimKiem;
+        private DevExpress.XtraEditors.SimpleButton mnuBCHangTon;
+        private DevExpress.XtraEditors.SimpleButton mnuBCDoanhThu;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
+
